668714661a15323d49a949821abc5979ef10f42f
sakshatl <voteforsakshat@gmail.com>
2023-12-31 23:41:22 +0530

    heap: max binary heap sink down

--- src/data-structures/heaps/max-binary-heap.ts
+++ src/data-structures/heaps/max-binary-heap.ts
@@ -25,9 +25,65 @@ class MaxBinaryHeap<T> {
     }
   }
 
+  private sinkDown() {
+    let index = 0;
+    const length = this.collection.length;
+    const currentParent = this.collection[index];
+
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let swap: number | null = null;
+
+      let leftChild: T | undefined = undefined;
+      let rightChild: T | undefined = undefined;
+
+      // check if the left and right child indexes are in bounds
+      if (leftChildIndex < length) {
+        leftChild = this.collection[leftChildIndex];
+        if(leftChild > currentParent) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.collection[rightChildIndex];
+
+        const cond1 = swap === null && rightChild > currentParent;
+        const cond2 = swap !== null && leftChild !== undefined && leftChild !== null && rightChild > leftChild;
+
+        if(cond1 || cond2) {
+          swap =  rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.collection[index] = this.collection[swap];
+      this.collection[swap] = currentParent;
+      index = swap;
+    }
+
+  }
+
   insert(data: T) {
     this.collection.push(data);
+
+    // bubble up
     this.bubbleUp();
+  }
+
+  extractMax() {
+    if (!this.collection.length) return undefined;
+
+    const max = this.collection[0];
+    const end = this.collection.pop() as T;
+    this.collection[0] = end;
+
+    // sink down
+    this.sinkDown();
+
+    return max;
+
   }
 
   print() {
